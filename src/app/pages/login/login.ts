285a02457b907c4b114656e505c8f3ae8aa22a94
Alessandra Brito Paim <alebritinha@gmail.com>
2026-05-24 00:40:41 -0300

    service

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 //import { AuthService } from '../../services/auth.service';
 
