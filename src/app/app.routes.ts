0b339b8b44a062d4c056ac83678f8b78fb875346
Alessandra Brito Paim <alebritinha@gmail.com>
2026-05-24 01:17:51 -0300

    home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from './pages/login/login';
 import { CadastroComponent } from './pages/cadastro/cadastro';
+import { HomeComponent } from './pages/home/home';
 
 
 export const routes: Routes = [
@@ -15,4 +16,8 @@ export const routes: Routes = [
     path: 'cadastro',
     component: CadastroComponent
   },
+   {
+    path: 'home',
+    component: HomeComponent
+  }
 ];
